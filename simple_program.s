c02dd48b53af7f02d07f2533b25e39c91b1fab06
Kevin Kuwata <kevinkuwata@icloud.com>
2018-03-01 19:33:24 -0700

    not a very instructive problem, and the question is  atrick question, we will never reach label C. but at least I commited today. on this day I studied pds

--- simple_program.s
+++ simple_program.s
@@ -1,14 +1,27 @@
 	.text
-	.equ	LEDs,		0xFF200000
-	.equ	SWITCHES,	0xFF200040
+.include "nios_macros.s"
 	.global _start
 _start:
-	movia	r2, LEDs		# Address of LEDs         
-	movia	r3, SWITCHES	# Address of switches
+	orhi		r2, r0, 0xfafa
+	ori			r2, r2, 0xafaf
+	add 		r3, r0, r0
+	andhi 		r8, r0, 0
+	addi		r8,r8,1
+	addi 		r9, r0, 0
+	addi 		r9, r9, 1
+	xor 		r11, r11, r11
+	nor 		r2, r2, r2
 
-LOOP:
-	ldwio	r4, (r3)		# Read the state of switches
-	stwio	r4, (r2)		# Display the state on LEDs
-	br		LOOP
+A: 	
+	and r10, r8, r2
+	beq r10, r0, B
+	addi r3, r3, 1
+
+B: 	srl r2, r2, r9
+	addi r11, r11, 1
+	bne r2, r0, A
+
+C:
+	br C
 
 	.end
